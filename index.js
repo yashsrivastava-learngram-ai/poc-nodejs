48ae105a98150280e137a652cbd4e077996c16df
yashsrivastava-learngram-ai <66585395+yashsrivastava-learngram-ai@users.noreply.github.com>
2020-06-24 00:07:24 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 const PORT = 8080;
 const HOST = '0.0.0.0';
 
-app.get('/', (req, res) => res.send('Hello World New to Github actions!'))
+app.get('/', (req, res) => res.send('Hello World New to Github actions Now!'))
 
 app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
