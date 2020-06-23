57bef27612e72780abf052268440d5886589f1cc
Darkshredder <srivastavayash58@gmail.com>
2020-06-23 23:38:25 +0530

    push

--- index.js
+++ index.js
@@ -1,11 +1,7 @@
 const express = require('express')
 const app = express()
-<<<<<<< HEAD
-const port = 4000
-=======
 const PORT = 8080;
 const HOST = '0.0.0.0';
->>>>>>> 0687875f32dcb15ae50a0a0a8bf7dae15a9d517d
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
